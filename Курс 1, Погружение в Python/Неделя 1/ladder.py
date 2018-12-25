c592c3930f12a86111281aca57af8790509b145f
valery <099500@gmail.com>
2018-12-25 22:03:49 +0500

    done

--- Курс 1, Погружение в Python/Неделя 1/ladder.py
+++ Курс 1, Погружение в Python/Неделя 1/ladder.py
@@ -1,5 +1,11 @@
 import sys
 num_steps = int(sys.argv[1])
 
-for i in range(num_steps, 0, -1):
-    print(" " * i, "#" * (num_steps-i))
+for i in range(num_steps-1, -1, -1):
+    print(" " * (i) + "#" * (num_steps-i))
+
+
+"""
+for i in range(num_steps):
+    print(" " * (num_steps - i - 1), "#" * (i + 1), sep="")
+"""
--- Курс 1, Погружение в Python/Неделя 1/solution.py
+++ Курс 1, Погружение в Python/Неделя 1/solution.py
@@ -7,3 +7,5 @@ sum = 0
 for digit in digit_string:
     sum += int(digit)
 print(sum)
+
+# print(sum([int(x) for x in sys.argv[1]]))
