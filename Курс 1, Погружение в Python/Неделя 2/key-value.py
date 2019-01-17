e8175543798c6f05b307fa6489a43be158546a08
victoriousvalery <099500@gmail.com>
2019-01-17 14:23:11 +0500

    1

--- Курс 1, Погружение в Python/Неделя 2/key-value.py
+++ Курс 1, Погружение в Python/Неделя 2/key-value.py
@@ -9,9 +9,42 @@ parser.add_argument("--value")
 args = parser.parse_args()
 
 storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
-with open(storage_path, 'w+') as f:
-    if args.key and not args.value:  # чтение. передан только ключ без значения
-        pass
-    elif args.key and args.value:  # запись. переданы и ключ, и значение
-        f.write(json.dumps({args.key: args.value}))
-    print(f.read())
+if not os.path.isfile(storage_path):
+    f = open(storage_path, 'w')
+    f.close()
+
+if args.key and not args.value:  # чтение. передан только ключ без значения
+    data_read = None
+    with open(storage_path, 'r') as f:
+        try:
+            data_read = json.loads(f.read())
+        except:
+            print(None)
+        else:
+            if args.key in data_read.keys() and data_read:
+                data_read = data_read[args.key]
+                data_read = set(data_read)
+                output_string = ", ".join(data_read)
+                print(output_string)
+            else:
+                print(None)
+
+elif args.key and args.value:  # запись. переданы и ключ, и значение
+    data = None
+    data_write = None
+    args_write = None
+    with open(storage_path, 'r') as f:  # надо прочитать предварительно, потому что иначе перезапишется файл
+        if not f.read() == "":
+            f.seek(0)
+            data = json.loads(f.read())
+    with open(storage_path, 'w') as f:
+        if data:
+            if args.key in data.keys() and args.value not in data.values():
+                data[args.key].append(args.value)
+                f.write(json.dumps(data))
+            elif args.key not in data.keys() and args.value not in data.values():
+                args_write = {args.key: [args.value]}
+                data_write = dict(list(data.items())+list(args_write.items()))
+                f.write(json.dumps(data_write))
+        else:
+            f.write(json.dumps({args.key: [args.value]}))
